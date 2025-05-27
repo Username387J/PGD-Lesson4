2b31d928c4bc1836fd96c72c09e0d5f10420a736
Username387J <jdeleijagbulu@gmail.com>
2025-05-26 19:41:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+\import pygame
 pygame.init()
 
 screen=pygame.display.set_mode((600,600))
@@ -27,7 +27,7 @@ class Circle:
         self.screen=screen
         self.wid=wid
 
-    def drawCircel(self):
+    def drawCircle(self):
         pygame.draw.circle(self.screen,self.color,self.position,self.rad,self.wid)
     
     def grow(self,r):
@@ -37,13 +37,7 @@ class Circle:
 blueCircle=Circle(blue,position,radius+90,4)
 redCircle=Circle(red,position,radius+50,3)
 greenCircle=Circle(green,position,radius+70,2)
-pinkCircle=Circle(black,position,radius+60,1)  
-
-
-    
-
-
-
+pinkCircle=Circle(pink,position,radius+60,1)  
 
 
 while True:
@@ -51,4 +45,29 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+#Mouse is clicked
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            blueCircle.drawCircle()
+            redCircle.drawCircle()
+            greenCircle.drawCircle()
+            pinkCircle.drawCircle()
+            pygame.display.update()
+#mouse is released            
+        if event.type == pygame.MOUSEBUTTONUP:
+            blueCircle.grow(2)
+            redCircle.grow(2)
+            greenCircle.grow(2)
+            pinkCircle.grow(2)
+            pygame.display.update()
+#mouse is moving            
+        if event.type == pygame.MOUSEMOTION:
+            pos=pygame.mouse.get_pos()
+            blackCircle=Circle(black,pos,5)
+            blackCircle.drawCircle()
+            pygame.display.update()
+
+
+
+        
+
 
